573ea04b8966461cd1277d934fec27505a959c36
yuhei-18 <kasaiyuhei8@gmail.com>
2021-12-27 11:47:55 +0900

    feat: api サーバーと通信を確認する

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,11 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: 'gatsby-plugin-apollo',
+            options: {
+                uri: 'http://127.0.0.1:5000/graphql'
+            }
+        }
     ]
 }
