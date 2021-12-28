aba496596eedf37d6e61e7c64d2b3d17bdfc0f2b
yuhei-18 <kasaiyuhei8@gmail.com>
2021-12-28 12:03:57 +0900

    feat: tailwindcss のインストールと設定

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
+import './src/styles/global.css'
 export { wrapRootElement } from './src/apollo/provider'
--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,6 @@ module.exports = {
             }
         },
         'gatsby-plugin-sass',
+        'gatsby-plugin-postcss',
     ]
 }
