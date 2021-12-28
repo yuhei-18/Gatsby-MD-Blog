08915b88893d8b0ebfcb5d653182357a74726882
yuhei-18 <kasaiyuhei8@gmail.com>
2021-12-28 16:20:09 +0900

    feat: Post 詳細ページを作成

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,5 +1,9 @@
 import React from "react"
+import { useQuery } from "@apollo/client"
+import { POSTS } from "../../graphql/query"
 import Layout from "../../components/Layout"
+import ToMD from "../../components/ToMD"
+import { PostNodeType, PostType } from "../../types"
 
 type Pathname = {
   pathname: string
@@ -12,9 +16,17 @@ type PropsType = {
 const IndexPage: React.FC<PropsType> = (props) => {
   const { location } = props
   const id = location.pathname.split("/")[1]
+  const { loading, error, data } = useQuery(POSTS)
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>error: {error.message}</div>
+
+  const posts: PostNodeType = data.posts.edges.filter((post: PostNodeType) => post.node.id == id)
+  const post: PostType = posts[0].node
+
   return (
     <Layout>
-      {id}
+      <ToMD text={post.text} />
     </Layout>
   )
 }
